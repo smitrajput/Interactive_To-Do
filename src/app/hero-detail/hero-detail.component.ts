1550b7e50e3be87eade7e542e8456a8b7052af9d
Smit Rajput <smitrajputtd@gmail.com>
2019-03-21 17:36:48 +0530

    Enable CRUD on the list of heroes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -34,4 +34,9 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
 }
